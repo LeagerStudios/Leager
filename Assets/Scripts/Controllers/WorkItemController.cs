61c7752279020b36061a67dd695fca5c63f67385
LeagerStudios <111186389+LeagerStudios@users.noreply.github.com>
2024-02-25 15:05:52 +0100

    Update WorkItemController.cs

--- Assets/Scripts/Controllers/WorkItemController.cs
+++ Assets/Scripts/Controllers/WorkItemController.cs
@@ -36,7 +36,7 @@ public class WorkItemController : MonoBehaviour {
                         if (canInteract[tileSelected] == true)
                         {
                             workIcon.GetComponent<SpriteRenderer>().sprite = spritesRenders[tileSelected];
-                            if (GInput.GetMouseButtonDown(1) || (GInput.leagerInput.platform == "Mobile" && !GameManager.gameManagerReference.cancelPlacing))
+                            if (GInput.GetMouseButtonDown(1) || (GInput.leagerInput.platform == "Mobile" && !GameManager.gameManagerReference.cancelPlacing && GInput.GetMouseButtonDown(0)))
                             {
                                 DisplayForTile(tileSelected, rayHit);
                             }
