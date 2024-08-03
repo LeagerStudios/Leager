a8f6d762dbcf48dbee471071b004805fc8e93907
LeagerStudios <leagerstudios@gmail.com>
2024-08-03 11:19:48 +0200

    ahhgg

---
+++ Assets/Scripts/Controllers/Nodes/Node.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> connections;
+    public int connectionLimit;
+
+    public NodeConnectionStatus AddConnection(Node connection)
+    {
+        NodeConnectionStatus status = new NodeConnectionStatus();
+        status.Initialize();
+
+        if(connection != this)
+        {
+            if(typeof(NodeSource) == connection.GetType())
+            {
+                status.closed = true;
+            }
+            else
+            {
+
+            }
+        }
+
+        return status;
+    }
+}
+
+public struct NodeConnectionStatus
+{
+    public bool closed;
+
+    public void Initialize()
+    {
+        closed = false;
+    }
+}
---
+++ Assets/Scripts/Controllers/Nodes/NodeSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSource : Node
+{
+    public float power = 0f;
+    public float targetPower = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        power = targetPower;
+    }
+}
