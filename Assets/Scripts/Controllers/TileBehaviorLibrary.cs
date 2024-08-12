14f1cfec382066b76bd568ec9fb829626ee9ac4f
LeagerStudios <leagerstudios@gmail.com>
2024-08-12 13:38:54 +0200

    Kuak 2

---
+++ Assets/Scripts/Controllers/TileBehaviorLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviorLibrary : MonoBehaviour
+{
+    public static GameObject[] library;
+
+    public GameObject[] lib;
+
+    private void Start()
+    {
+        library = lib;
+    }
+}
