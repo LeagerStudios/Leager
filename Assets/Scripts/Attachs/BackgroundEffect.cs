fb05d25ad1e74eb08c50395d64cce5532409129d
LeagerStudios <leagerstudios@gmail.com>
2024-10-31 15:26:39 +0100

    bsssfddzsad

---
+++ Assets/Scripts/Attachs/BackgroundEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundEffect : MonoBehaviour
+{
+    void Update()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.position *= 0.9f;
+        transform.localPosition = new Vector2(transform.localPosition.x % 1, transform.localPosition.y % 1);
+    }
+}
