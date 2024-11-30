72de2962d868521e38804a1d3874e21227657cf2
LeagerStudios <leagerstudios@gmail.com>
2024-11-30 16:22:18 +0100

    The Real SS

---
+++ Assets/Scripts/Main/LoadingSceneTitle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneTitle : MonoBehaviour
+{
+
+    [SerializeField] Canvas canvas;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+        {
+            transform.parent = canvas.transform;
+            transform.SetAsLastSibling();
+            SceneManager.sceneLoaded -= OnSceneLoad;
+        }
+        else if (scene.name != "LoadScreen")
+        {
+            SceneManager.sceneLoaded -= OnSceneLoad;
+            Destroy(gameObject);
+        }
+    }
+}
